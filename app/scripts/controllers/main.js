33c4a1848433968c486f276e0a7651db61316133
Josh Click <joshuaqian8@gmail.com>
2014-01-16 00:32:43 -0800

    better way to tell what is content for active page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,7 +12,7 @@ angular.module('miniweeblyApp')
       title: 'Another'
     }
   ];
-  $scope.content = [{
+  $scope.contents = [{
       pageID: null,
       title: 'Add Title Here',
       text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
@@ -26,7 +26,10 @@ angular.module('miniweeblyApp')
       text: 'Real content yo'
     },
   ];
-  $scope.activeContent = $scope.content[0];
+
+  $scope.isActiveContent = function (content) {
+    return content.pageID === $scope.activePage;
+  };
 
   $scope.deletePage = function (id) {
     $scope.pages = $scope.pages.filter(function (page) {
@@ -47,9 +50,6 @@ angular.module('miniweeblyApp')
   };
   $scope.activatePage = function(id) {
     $scope.activePage = id;
-    $scope.activeContent = $scope.content.filter(function (d) {
-      return d.pageID === $scope.activePage;
-    })[0];
   };
 
   $scope.gridToggle = false; // need add hover state
