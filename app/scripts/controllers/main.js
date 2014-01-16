47fb062af8b509568f66082fe487f385b7bac52f
Josh Click <joshuaqian8@gmail.com>
2014-01-16 11:12:01 -0800

    move default page to its own value

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,19 +3,20 @@
 angular.module('miniweeblyApp')
 .controller('MainCtrl', function ($scope) {
   $scope.currPageID = 1;
+  $scope.defaultPage = {
+    pageID: false,
+    pageName: 'Page',
+    contents: [
+      {
+        type: 'title',
+        value: 'Add Title Here',
+      },{
+        type: 'text',
+        value: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      }
+    ]
+  };
   $scope.pages = [{
-      pageID: null,
-      pageName: 'Page',
-      contents: [
-        {
-          type: 'title',
-          value: 'Add Title Here',
-        },{
-          type: 'text',
-          value: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-        }
-      ]
-    },{
       pageID: 0,
       pageName: 'Page',
       contents: [
