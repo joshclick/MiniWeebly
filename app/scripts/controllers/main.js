9f7073493a389fa923f4d7e43fed86baa34e4ac4
Josh Click <joshuaqian8@gmail.com>
2014-01-15 17:05:27 -0800

    hooking up dummy content data with individual pages
    asdf

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,16 +7,27 @@ angular.module('miniweeblyApp')
   $scope.gridToggle = false; // need add hover state
   $scope.pages = [{
       id: 0,
-      name: 'Page'
+      title: 'Page'
     },{
       id: 1,
-      name: 'Another'
+      title: 'Another'
     }
   ];
-  $scope.defaultContent = {
-    title: 'Add Title Here',
-    text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  };
+  $scope.content = [{
+      pageID: null,
+      title: 'Add Title Here',
+      text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },{
+      pageID: 0,
+      title: 'Page 1',
+      text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },{
+      pageID: 1,
+      title: 'Page 2',
+      text: 'Real content yo'
+    },
+  ];
+  $scope.activePage = $scope.content[0];
 
   $scope.deletePage = function (id) {
     $scope.pages = $scope.pages.filter(function (page) {
@@ -28,7 +39,7 @@ angular.module('miniweeblyApp')
     if ($scope.pages.length < 7) {
       $scope.pages.push({
         id: ++$scope.currID,
-        name: $scope.newPageText
+        title: $scope.newPageText
       });
       $scope.newPageText = '';
     } else {
@@ -37,5 +48,8 @@ angular.module('miniweeblyApp')
   };
   $scope.activatePage = function(id) {
     $scope.activePage = id;
+    $scope.activeContent = $scope.content.filter(function (d) {
+      return d.pageID === $scope.activePage;
+    })[0];
   };
 });
