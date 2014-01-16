f432a6226e13ea3181729d5e896858988ba7c168
Josh Click <joshuaqian8@gmail.com>
2014-01-16 11:07:44 -0800

    completely rework pages model

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,34 +2,46 @@
 
 angular.module('miniweeblyApp')
 .controller('MainCtrl', function ($scope) {
-  $scope.currID = 1;
-  $scope.activePage = 0;
+  $scope.currPageID = 1;
   $scope.pages = [{
-      id: 0,
-      title: 'Page'
-    },{
-      id: 1,
-      title: 'Another'
-    }
-  ];
-  $scope.contents = [{
       pageID: null,
-      title: 'Add Title Here',
-      text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      pageName: 'Page',
+      contents: [
+        {
+          type: 'title',
+          value: 'Add Title Here',
+        },{
+          type: 'text',
+          value: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+      ]
     },{
       pageID: 0,
-      title: 'Page 1',
-      text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      pageName: 'Page',
+      contents: [
+        {
+          type: 'title',
+          value: 'Page 1',
+        },{
+          type: 'text',
+          value: 'Lala lots of content. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. \n Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes. nascetur ridiculus mus. Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+      ]
     },{
       pageID: 1,
-      title: 'Page 2',
-      text: 'Real content yo'
+      pageName: 'Another',
+      contents: [
+        {
+          type: 'title',
+          value: 'Page 2',
+        },{
+          type: 'text',
+          value: 'Real content yo'
+        }
+      ]
     },
   ];
-
-  $scope.isActiveContent = function (content) {
-    return content.pageID === $scope.activePage;
-  };
+  $scope.activePage = $scope.pages[1];
 
   $scope.deletePage = function (id) {
     $scope.pages = $scope.pages.filter(function (page) {
@@ -40,7 +52,7 @@ angular.module('miniweeblyApp')
   $scope.addPage = function (title) {
     if ($scope.pages.length < 7 && title.length > 0) {
       // add to pages
-      var newPageID = ++$scope.currID;
+      var newPageID = ++$scope.currPageID;
       $scope.pages.push({
         id: newPageID,
         title: title
@@ -58,7 +70,13 @@ angular.module('miniweeblyApp')
     }
   };
   $scope.activatePage = function(id) {
-    $scope.activePage = id;
+    $scope.activePage = $scope.pages.filter(function (d) {
+      return d.pageID === id;
+    })[0];
+  };
+
+  $scope.editContent = function(content) {
+    console.log(content);
   };
 
   $scope.gridToggle = false; // need add hover state
